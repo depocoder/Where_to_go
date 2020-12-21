e8695e86f053c03894cce1304f31dd3ef6be766f
herypank <60302361+herypank@users.noreply.github.com>
2020-12-21 15:43:31 +0300

    Поправил ошибку в JS коде

--- places/views.py
+++ places/views.py
@@ -3,6 +3,5 @@ from places.models import Place
 
 
 def index(request):
-    print(len(Place.objects.all()))
     return render(
         request, "index.html", context={'places': Place.objects.all()})

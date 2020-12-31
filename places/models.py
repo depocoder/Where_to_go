ad4a80ccc43b4832afe4ce59b30fb842d2f38c03
herypank <60302361+herypank@users.noreply.github.com>
2021-01-01 02:28:19 +0300

    Подключил WYSIWYG

--- places/models.py
+++ places/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.safestring import mark_safe
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=100)
     description_short = models.CharField('Короткое описание', max_length=100)
-    description_long = models.TextField("Подробное описание")
+    description_long = HTMLField("Подробное описание")
     lat = models.FloatField('Широта')
     lon = models.FloatField('Долгота')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from places import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('place/<place_id>/', views.show_place, name="place"),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

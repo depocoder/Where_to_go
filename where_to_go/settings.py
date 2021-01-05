5c59c89f6daf7686425139058614bc9a344ade66
herypank <60302361+herypank@users.noreply.github.com>
2021-01-05 22:43:01 +0300

    Исправил переменные SECRET_KEY, DEBUG

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = "sfjsldfhl:;?*213HASJKDHDKJAgJWEDKJASKLDWLHRhehkleh223!&*^&*#~"
+SECRET_KEY = os.getenv("SECRET_KEY", "REPLACE_ME")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", "true").lower() in ['yes', '1', 'true']
 
 
 ALLOWED_HOSTS = [

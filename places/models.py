d9dcde98f82d76b5b765f708d154a7fc7c20d871
herypank <60302361+herypank@users.noreply.github.com>
2021-01-10 11:16:27 +0300

    Разрешил полям short_description & long_description быть пустыми

--- places/models.py
+++ places/models.py
@@ -5,8 +5,9 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=100)
-    short_description = models.TextField('Короткое описание')
-    long_description = HTMLField("Подробное описание")
+    short_description = models.TextField(
+        'Короткое описание', null=True, blank=True)
+    long_description = HTMLField("Подробное описание", null=True, blank=True)
     lat = models.FloatField('Широта')
     lon = models.FloatField('Долгота')
 

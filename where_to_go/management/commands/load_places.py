4d72c972dd55d33bf4e135207cf29fa073048377
herypank <60302361+herypank@users.noreply.github.com>
2021-01-18 11:17:47 +0300

    Настроил метод get_or_create

--- where_to_go/management/commands/load_places.py
+++ where_to_go/management/commands/load_places.py
@@ -20,10 +20,12 @@ class Command(BaseCommand):
         place = response.json()
         place_obj, created = Place.objects.get_or_create(
             title=place['title'],
-            description_short=place['description_short'],
-            description_long=place['description_long'],
-            lat=place['coordinates']['lat'],
-            lon=place['coordinates']['lng'],
+            
+            defaults={
+                "short_description": place['description_short'],
+                "long_description": place['description_long'],
+                "lat": place['coordinates']['lat'],
+                "lon": place['coordinates']['lng'],},
         )
 
         for index, image_url in enumerate(place['imgs']):
